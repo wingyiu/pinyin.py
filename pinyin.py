0cb8f9e94b9d4618d9696bbe715849de1a0f972a
wingyiu <wyiu.lee@gmail.com>
2013-09-04 16:05:33 +0800

    小修改

--- pinyin.py
+++ pinyin.py
@@ -2,25 +2,20 @@
 # -*- coding:utf-8 -*-
 
 """
-    based on pinyin.py V0.9         Author:  cleverdeng     E-mail:clverdeng@gmail.com
-    #https://github.com/pinyin.py.git
+    convert string contain Hans/Hant characters and ASCII printable characters
+    to english characters
+    
+    把包含中文简繁体和英文字母符号的字符串转化成字母串
+    要求中文字符是utf-8编码
    
 """
 
-__author__="Qingfeng Xia"
+__author__="cleverdeng, qingfengXia"
 __version__ = '0.9a'
 __all__ = ["Pinyin"]
-#https://github.com/qingfengxia/pinyin.py.git
+
 
 import os.path
-
-"""
-def convert_dict(input ):
-    with file(dict_file) as f_obj:
-        for f_line in f_obj.readlines():
-            line = f_line.split('=') # try and except is costly, just using normal reading as data input is determinate!
-            self.word_dict[line[0]] = line[1]
-"""
 
 class Pinyin(object):  #naming 
     def __init__(self,  encoding="utf-8",dict_file='pinyin.data'):
@@ -32,6 +27,9 @@ class Pinyin(object):  #naming
         self.loaddict()
 
     def loaddict(self):
+        """
+        加载字典
+        """
         if not os.path.exists(self.dict_file):
             raise IOError("NotFoundFile")
 
@@ -41,26 +39,32 @@ class Pinyin(object):  #naming
                 self.word_dict[line[0]] = line[1]
 
 
-    def hanzi2pinyin(self, string="", showingtone=True, split=None):
-        """  add a switch to show tone number: showingtone
-        repr:   
-        split :  controlled by switch now
+    def hanzi2pinyin(self, string="", showingtone=False, split=None):
+        """  
+        把中文字符串转换为拼音字符串，英文字母不转
+        string: 原含中文字的字符串
+        showingtone: 是否输出以1234代表的音标
+        split: 拼音的分隔字符，如' '、'-'
         """
         result = []
         if not isinstance(string, unicode):
             string = string.decode(self.encoding) # it will not work for windows cmd prompt! if fixed encoding as utf-8
         
         for char in string:
-            key = '%X' % ord(char)
-            if showingtone:
-                result.append(self.word_dict.get(key, char).split()[0].lower())
+            intv = ord(char)
+            if intv >31 and intv < 127: #跳过英文字母符号
+                result.append(char.lower())
             else:
-                result.append(self.word_dict.get(key, char).split()[0][:-1].lower())
+                key = '%X' % intv
+                if showingtone:
+                    result.append(self.word_dict.get(key, char).split()[0].lower())
+                else:
+                    result.append(self.word_dict.get(key, char).split()[0][:-1].lower())
         #        
-        if split!=None and type(split)==type(' '):
+        if split != None and type(split)==type(' '):
             return split.join(result)
         else:
-            return result
+            return ''.join(result)
 
 
     def hanzi2pinyin_split(self, string="", split=""):   # this could be merged into hanzi2pinyin()
@@ -82,16 +86,16 @@ def test_console(encoding='cp936'):
 
 if __name__ == "__main__":
     test = Pinyin()
-    #test.load_word()  # it shuld be done in ctor()
-    import os
-    #
     print "test with utf8 console encoding"
-    string = "钓鱼岛是中国的"  #utf8  simplified chinese
-    print "in: %s" % string
-    print "out: %s" % str(test.hanzi2pinyin(string, showingtone=True))
-    print "out: %s" % test.hanzi2pinyin_split(string, split="-")
-    #
-    str2="釣魚島是台灣的也是中國的" #utf8 traditional chinese
+    
+    #utf8  simplified chinese
+    string = "共产党一党专政何谈民族？hehe"  
+    print "example 1 in: %s" % string
+    print "example 1 out: %s" % test.hanzi2pinyin(string, showingtone=True)
+    print "example 1 showingtone+split: %s" % test.hanzi2pinyin(string, showingtone=False, split=' ')
+    
+    #utf8 traditional chinese
+    str2="釣魚島是台灣的也是中國的" 
     print "in: %s" % str2
     print "out: %s" % str(test.hanzi2pinyin(str2, showingtone=True))
     
